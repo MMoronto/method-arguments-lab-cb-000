0aaf5502bd386bee69775a9bf9c844066063e956
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-01-15 20:52:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ end
 
 language = "Ruby"
 
-def introduction_with_language(name)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
